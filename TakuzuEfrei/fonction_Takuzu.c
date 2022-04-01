a914e03ccf8398ffcac15b7b1df09f895f62d81e
SuperSo6 <89916346+SuperSo6@users.noreply.github.com>
2022-04-01 19:59:24 +0200

    Add files via upload

---
+++ TakuzuEfrei/fonction_Takuzu.c
@@ -0,0 +1,36 @@
+//
+// Created by Romain on 01/04/2022.
+//
+
+#include "fonction_Takuzu.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void choix_joueur(){
+
+    /* Mettre sleep(2); */
+    printf("Pour acc%cder aux r%cgles entrez : regles \n",130,138);
+
+
+    printf("Pour r%csoudre une grille entrez : resoudre \n",130);
+
+
+    printf("Pour r%csoudre une grille automatiquement entrez : auto \n",130);
+
+
+    printf("Pour g%cn%crer une grille entrez : generer \n",130,130);
+
+
+    printf("Votre choix : ");
+}
+
+
+
+void regles(){
+    printf("R%cgles num%cros 1 : Dans une ligne, il doit y avoir autant de 0 que de 1            \n",138,130);
+    printf("R%cgles num%cros 2 : Dans une colonne, il doit y avoir autant de 0 que de 1          \n",138,130);
+    printf("R%cgles num%cros 3 : Il ne peut pas y avoir deux lignes identiques dans une grille   \n",138,130);
+    printf("R%cgles num%cros 4 : Il ne peut pas y avoir deux colonnes identiques dans une grille \n",138,130);
+    printf("R%cgles num%cros 5 : Dans une ligne ou une colonne, il ne peut y avoir plus de deux 0 ou deux 1 %c la suite \n"
+           "(on ne peut pas avoir trois 0 de suite ou trois 1 de suite) \n",138,130,133);
+}
---
+++ TakuzuEfrei/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "fonction_Takuzu.h"
+#include <string.h>
+
+
+
+int main() {
+
+    /* VARIABLES */
+    char choix[32];
+
+
+    /* DEBUT AFFICHAGE */
+    printf("Bienvenue dans le jeu du Takuzu ! \n");
+
+    sleep(1);
+
+    choix_joueur();
+
+    gets(choix);
+
+    /* Premier Choix du joueur */
+    if (strcmp(choix,"regles")==0){                     /* Affiche les règles */
+        printf("\n");
+        regles();
+
+    }else if (strcmp(choix,"resoudre")==0){            /* Partie où le joueur résout lui meme la grille de Takuzu */
+        printf("\n");
+        printf("Resoudre");
+        /* resoudre() */
+
+    } else if (strcmp(choix,"auto")==0){                /* Partie où l'ordinateur  résout  la grille de Takuzu */
+        printf("\n");
+        printf("Auto");
+        /* auto() */
+
+    } else if (strcmp(choix,"generer")==0){             /* Partie pour générer une grille automatiquement */
+        printf("\n");
+        printf("Generer");
+        /* generer() */
+
+    } else {                                           /* Relance le programme si l'utilisateur s'est trompé */
+        printf("\n");
+        printf("Ce choix n'est pas disponible \n");
+        printf("\n");
+        main();
+    }
+
+    return 0;
+}
